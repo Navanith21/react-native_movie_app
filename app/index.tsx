e4fb6f8b1b9e21a362819e38207967361efbfe4d
Navanith21 <navanithsenthil2006@gmail.com>
2025-05-21 10:21:39 +0530

    updated project

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View } from "react-native";
-
+import "./global.css"
 export default function Index() {
   return (
     <View
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>WELCOME!</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
